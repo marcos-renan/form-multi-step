4a972fbea1c0afced9d768afebb2e12a44bb7fcc
Marcos <devmarcos7@gmail.com>
2024-10-24 10:03:52 -0300

    Criando o projeto e iniciando

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); //iniciando a sessão
+
+if(!isset($_SESSION['etapa'])) { //se não existir a sessão etapa
+  $_SESSION('etapa'); //cria a sessão etapa
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulário</title>
+</head>
+<body>
+
+</body>
+</html>
